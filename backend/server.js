bdf3f3ff9ab3d1573282f9df6958b39b1afb1d2c
Yogeshwar2601 <yogeshwardadwal26@gmail.com>
2026-04-09 16:22:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,7 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: "https://blog-platform-oisqskr36-yogeshwars-projects-79f9a555.vercel.app",
-    credentials: true,
+    origin: "*",
 }));
 app.use(errorHandler);
 const __filename = fileURLToPath(import.meta.url);
